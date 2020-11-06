9afee7a1b2bc751afa4129fbbcd0ca7514081404
DannJohnrem <jonrhem10@gmail.com>
2020-11-06 18:55:57 +0800

    change query builder to standard query

--- application/models/Users_model.php
+++ application/models/Users_model.php
@@ -5,9 +5,16 @@
 			$this->load->database();
 		}
 
-		public function login($email, $password){
-			$query = $this->db->get_where('users', array('email'=>$email, 'password'=>$password));
-			return $query->row_array();
+		// public function login($email, $password){
+		// 	$query = $this->db->get_where('users', array('email'=>$email, 'password'=>$password));
+		// 	return $query->row_array();
+		// }
+
+		public function login($email) {
+			
+			$query = $this->db->query("SELECT `email`, `password`, `fname` FROM `users` WHERE `email` = ?", array( $email ));
+
+			return $query->result_array();
 		}
 
 	}
