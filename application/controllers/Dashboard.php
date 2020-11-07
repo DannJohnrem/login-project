9e0f1507304b44f8b35ae8b1c10c63a2db42cb51
DannJohnrem <jonrhem10@gmail.com>
2020-11-07 23:48:41 +0800

    added new controller

---
+++ application/controllers/Dashboard.php
@@ -0,0 +1,26 @@
+<?php 
+
+class Dashboard extends CI_Controller {
+
+    public function home() {
+
+		//load session library
+		$this->load->library('session');
+
+		//restrict users to go to home if not logged in
+		if($this->session->userdata('user')) {
+			$this->load->view('dashboard');
+		}
+		else{
+			redirect('/');
+		}
+		
+	}
+}
+
+
+
+
+
+
+?>
